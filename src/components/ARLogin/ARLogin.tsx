f326aea3cd23164b31634fff7c5f6b313e4363c3
abdulrahmanabdullah <nfs056@gmail.com>
2022-09-19 23:07:17 +0300

    add state to swtich form

--- src/components/ARLogin/ARLogin.tsx
+++ src/components/ARLogin/ARLogin.tsx
@@ -1,27 +1,32 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 interface ARLoginProps{
 
 }
 const ARLogin = () => {
+  const [isRegister, setIsRegister] = useState(false);
+
+  const switchForm = () =>  setIsRegister( (prevState)=> !prevState)
+
+  
   return (
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
               <div className="w-full max-w-md space-y-8">
-                            <div>
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-               "Sign in to your account"
-            </h2>
+                <div>
+                <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                    {isRegister ?"Sign up new user": "Sign in to your account"}
+                </h2>
             <p className="mt-2 text-center text-sm text-gray-600">
-               "Don't have account?"
+              {isRegister ? "Already have account?" : "Don't have account?"}
               <button
                 className="font-medium text-indigo-600 background-transparent"
-                onClick={()=> console.log("your component clicked")}
+                onClick={switchForm}
               >
-                &nbsp; "Sign Up here"
+               &nbsp;{isRegister ? " Sign In here" : "Sign Up here"}
               </button>
             </p>
           </div>
-              </div>
+          </div>
     </div>
   )
 }
